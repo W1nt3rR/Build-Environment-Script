906383cb686dbe94f634d18b5b24fd0212acaa15
nolinuxnoparty <nolinuxnoparty@gmail.com>
2013-07-18 23:41:14 +0530

    add script

---
+++ tr-be-script.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+clear
+echo "Linux Build Environment Setup Automater Script."
+echo "Written by nolinuxnoparty."
+echo "Released under supervision of Team Radium."
+read -p "Press [Enter] key to continue..."
+
+clear
+
+echo "Entering Downloads Directory"
+cd ~/Downloads
+read -p "Press [Enter] key to continue..."
+clear
+
+echo "Installing Python!"
+sudo apt-get install build-essential gcc
+wget http://www.python.org/ftp/python/2.5.6/Python-2.5.6.tgz
+tar -xvzf Python-2.5.6.tgz
+cd Python-2.5.6
+./configure --prefix=/usr/local/python2.5
+make
+sudo make install
+sudo ln -s /usr/local/python2.5/bin/python /usr/bin/python2.5
+cd ..
+
+read -p "Press [Enter] key to continue..."
+
+clear
+
+echo "Installing JDK 6!"
+wget  --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://download.oracle.com/otn-pub/java/jdk/6u45-b06/jdk-6u45-linux-x64.bin"
+chmod +x jdk-6u45-linux-x64.bin
+sudo ./jdk-6u45-linux-x64.bin
+sudo mv jdk1.6.0_45 /usr/lib/jvm/
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.6.0_45/bin/java 1
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk1.6.0_45/bin/javac 1
+sudo update-alternatives --install /usr/bin/javaws javaws /usr/lib/jvm/jdk1.6.0_45/bin/javaws 1
+sudo update-alternatives --install /usr/bin/jar jar /usr/lib/jvm/jdk1.6.0_45/bin/jar 1
+sudo update-alternatives --install /usr/bin/javadoc javadoc /usr/lib/jvm/jdk1.6.0_45/bin/javadoc 1
+java -version
+
+read -p "Press [Enter] key to continue..."
+
+clear
+
+echo "Installing GNU Make!"
+
+wget -o make.tar.gz http://ftp.gnu.org/gnu/make/make-3.81.tar.gz
+tar -xvzf make-3.81.tar.gz
+cd make-3.81
+./configure
+sudo make install
+cd ..
+
+read -p "Press [Enter] key to continue..."
+
+clear
+
+echo "Installing Required Packages!"
+
+apt-get install git-core gnupg flex bison gperf build-essential \
+zip curl zlib1g-dev libc6-dev libncurses5-dev x11proto-core-dev \
+libx11-dev libreadline6-dev libgl1-mesa-dev tofrodos python-markdown \
+libxml2-utils xsltproc pngcrush gcc-multilib lib32z1 schedtool
+
+read -p "Press [Enter] key to continue..."
+
+clear
+
+echo "Installing GIT!"
+
+sudo apt-get install git
+y
+
+echo "Installing ADB Drivers!"
+sudo apt-get install android-tools-adb
+
+read -p "Press [Enter] key to continue..."
+
+clear
+
+echo "Cleaning up downloaded shit!"
+rm Python-2.5.6.tgz
+rm Python-2.5.6
+rm make-3.81.tar.gz
+rm make-3.81
+rm jdk-6u45-linux-x64.bin
+read -p "Press [Enter] key to continue..."
+clear
+
+echo "Done!"
+
+echo "Cheers!"
+echo ""
+echo "Thanks for using this script!"
+echo "Now, Enjoy compiling roms/kernels :)"
+read -p "Press [Enter] key to exit..."
+exit
