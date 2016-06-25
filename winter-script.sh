5430a9011c20a3370af49a65563f89aea54e78b2
Ramiz <ramizsabovic@gmail.com>
2016-06-25 22:11:00 +0200

    Add liblz4-tool

--- winter-script.sh
+++ winter-script.sh
@@ -135,7 +135,7 @@ zip curl zlib1g-dev libc6-dev libncurses5-dev x11proto-core-dev \
 libx11-dev libreadline6-dev libgl1-mesa-dev tofrodos python-markdown \
 libxml2-utils xsltproc pngcrush gcc-multilib lib32z1 schedtool \
 libqt4-dev lib32stdc++6 libx11-dev:i386 g++-multilib lib32z1-dev \
-lib32ncurses5-dev lib32z-dev
+lib32ncurses5-dev lib32z-dev liblz4-tool
 
 if [ ${SKIP} = 1 ]; then
   echo "Unattended installation. skipping pause..."
