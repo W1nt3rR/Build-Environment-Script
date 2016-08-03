8f60fc508695f587344434b2d7d28acda0099e4c
Ramiz <ramizsabovic@gmail.com>
2016-08-03 08:18:08 +0200

    Update and Windows-ify
    And a little clean up

--- winter-script.sh
+++ winter-script.sh
@@ -54,9 +54,9 @@ echo
 echo "Installing Python!"
 echo
 sudo apt-get install build-essential gcc $PARAM
-wget http://www.python.org/ftp/python/3.6.0/Python-3.6.0a2.tgz
-tar -xvzf Python-3.6.0a2.tgz
-cd ~/winter-script/Python-3.6.0a2
+wget http://www.python.org/ftp/python/3.6.0/Python-3.6.0a3.tgz
+tar -xvzf Python-3.6.0a3.tgz
+cd ~/winter-script/Python-3.6.0a3
 ./configure --prefix=/usr/local/python3.6
 make -j${JOBS}
 sudo make install -j${JOBS}
@@ -74,9 +74,9 @@ clear
 echo
 echo "Installing CCache!"
 echo
-wget http://www.samba.org/ftp/ccache/ccache-3.2.5.tar.gz
-tar -xvzf ccache-3.2.5.tar.gz
-cd ~/winter-script/ccache-3.2.5
+wget http://www.samba.org/ftp/ccache/ccache-3.2.7.tar.gz
+tar -xvzf ccache-3.2.7.tar.gz
+cd ~/winter-script/ccache-3.2.7
 ./configure
 make -j${JOBS}
 sudo make install -j${JOBS}
@@ -134,8 +134,8 @@ sudo apt-get install git-core gnupg flex bison gperf build-essential \
 zip curl zlib1g-dev libc6-dev libncurses5-dev x11proto-core-dev \
 libx11-dev libreadline6-dev libgl1-mesa-dev tofrodos python-markdown \
 libxml2-utils xsltproc pngcrush gcc-multilib lib32z1 schedtool \
-libqt4-dev lib32stdc++6 libx11-dev:i386 g++-multilib lib32z1-dev \
-lib32ncurses5-dev lib32z-dev liblz4-tool
+libqt4-dev lib32stdc++6 g++-multilib lib32z1-dev \
+lib32ncurses5-dev lib32z1-dev liblz4-tool
 
 if [ ${SKIP} = 1 ]; then
   echo "Unattended installation. skipping pause..."
@@ -145,11 +145,11 @@ fi
 
 clear
 
-echo
-echo "Add  terminal to The mouse right button shortcut!"
-echo
-sudo apt-get install nautilus-open-terminal $PARAM
-nautilus -q
+#echo
+#echo "Add  terminal to The mouse right button shortcut!"
+#echo
+#sudo apt-get install nautilus-open-terminal $PARAM
+#nautilus -q
 
 echo
 echo "Installing GIT!"
@@ -234,17 +234,17 @@ fi
 
 clear
 
-echo
-echo "Installing DSIXDA's Android Kitchen"
-echo
-cd ~/winter-script
-wget https://github.com/dsixda/Android-Kitchen/archive/master.zip
-unzip master.zip
-mv -f Android-Kitchen-master ~/Android-Kitchen
-echo -e '\n#!/bin/bash\ncd ~/Android-Kitchen\n./menu' >> ~/Android-Kitchen/kitchen
-chmod 755 ~/Android-Kitchen/kitchen
-ln -s ~/Android-Kitchen/kitchen ~/bin/kitchen
-ln -s ~/Android-Kitchen/kitchen ~/Desktop/Android-Kitchen
+#echo
+#echo "Installing DSIXDA's Android Kitchen"
+#echo
+#cd ~/winter-script
+#wget https://github.com/dsixda/Android-Kitchen/archive/master.zip
+#unzip master.zip
+#mv -f Android-Kitchen-master ~/Android-Kitchen
+#echo -e '\n#!/bin/bash\ncd ~/Android-Kitchen\n./menu' >> ~/Android-Kitchen/kitchen
+#chmod 755 ~/Android-Kitchen/kitchen
+#ln -s ~/Android-Kitchen/kitchen ~/bin/kitchen
+#ln -s ~/Android-Kitchen/kitchen ~/Desktop/Android-Kitchen
 
 if [ ${SKIP} = 1 ]; then
   echo "Unattended installation. skipping pause..."
@@ -257,12 +257,12 @@ clear
 echo
 echo "Cleaning up temporary files..."
 echo
-rm -f ~/winter-script/Python-3.6.0a2.tgz
-sudo rm -rf ~/winter-script/Python-3.6.0a2
+rm -f ~/winter-script/Python-3.6.0a3.tgz
+sudo rm -rf ~/winter-script/Python-3.6.0a3
 rm -f ~/winter-script/make-4.2.1.tar.gz
 rm -rf ~/winter-script/make-4.2.1
-rm -f ~/winter-script/ccache-3.2.5.tar.gz
-rm -rf ~/winter-script/ccache-3.2.5
+rm -f ~/winter-script/ccache-3.2.7.tar.gz
+rm -rf ~/winter-script/ccache-3.2.7
 rm -rf ~/adt-bundle/adt-bundle-linux-x86_64-20140702
 rm -rf ~/adt-bundle/adt-bundle-linux-x86-20140702
 rm -f ~/adt-bundle/adt_x64.zip
